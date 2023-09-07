b91c37e93c8f2dd24a02c70602d73f62e308a094
ashraful-chowdhury <mohammad.ashraful@monstar-lab.com>
2023-09-07 11:32:26 +0600

    refactor: network module

--- lib/injection/network_module.dart
+++ lib/injection/network_module.dart
@@ -1,21 +1,34 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_template/presentation/app_flavor.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
+Dio _createBaseDio(AppFlavor flavor) {
+  final dio = Dio()
+    ..httpClientAdapter = HttpClientAdapter()
+    ..options.baseUrl = flavor.baseUrl;
+
+  return dio;
+}
+
 @module
-abstract class RegisterModule {
-  Dio get dio {
-    final dio = Dio(BaseOptions(baseUrl: 'https://favqs.com/api/'));
-    dio.interceptors.add(
-      PrettyDioLogger(
-          requestHeader: true,
-          requestBody: true,
-          responseBody: true,
-          responseHeader: false,
-          error: true,
-          compact: true,
-          maxWidth: 90),
-    );
+abstract class NetworkModule {
+  @singleton
+  Dio getDio(AppFlavor flavor) {
+    final dio = _createBaseDio(flavor)
+      ..interceptors.addAll(
+        [
+          PrettyDioLogger(
+            requestHeader: true,
+            requestBody: true,
+            responseBody: true,
+            responseHeader: false,
+            error: true,
+            compact: true,
+            maxWidth: 90,
+          ),
+        ],
+      );
 
     return dio;
   }
